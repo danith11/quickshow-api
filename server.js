87d4eb83d084fe572d055ff4a5800ba34fa69657
Yethum Danith <yethumdanith2002@gmail.com>
2025-07-24 13:20:44 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const port = 3000;
+
+//Middleware
+
+app.use(express.json());
+app.use(cors());
+
+// API Routes
+app.get("/", (req, res) => res.send("Server is live!!!"));
+
+app.listen(port, () =>
+  console.log(`Server is listing at http://localhost:${port}`)
+);
