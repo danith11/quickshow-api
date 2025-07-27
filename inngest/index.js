4ffa2354309b5c75922f47cda70bea228191ae01
Yethum Danith <yethumdanith2002@gmail.com>
2025-07-27 09:46:30 +0530

    Update inngest

--- inngest/index.js
+++ inngest/index.js
@@ -19,8 +19,35 @@ const syncUserCreation = inngest.createFunction(
       name: first_name + " " + last_name,
       image: image_url,
     };
-    await User.create(userData)
+    await User.create(userData);
   }
 );
 
-export const functions = [syncUserCreation];
+// Inngest function to delete user from database
+const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-with-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+    await User.findByIdAndDelete(id);
+  }
+);
+
+// Inngest function to update user from database
+const syncUserUpdation = inngest.createFunction(
+  { id: "update-user-with-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_addresses,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
+
+export const functions = [syncUserCreation, syncUserDeletion,syncUserUpdation];
