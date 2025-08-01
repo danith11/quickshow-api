fa074e7c321a8e93da40ec7e6df2cc892fee787a
Yethum Danith <yethumdanith2002@gmail.com>
2025-08-01 12:58:47 +0530

    Server.js updated

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ import cors from "cors";
 import "dotenv/config";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
+import { inngest, functions } from "./inngest/index.js";
 import connectDB from "./configs/db.js";
 import showRouter from "./routes/showRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 
 const app = express();
 const port = 3000;
@@ -21,7 +22,8 @@ app.use(clerkMiddleware());
 app.get("/", (req, res) => res.send("Server is live!!!"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.use("/api/show",showRouter)
+app.use("/api/show", showRouter);
+app.use("/api/booking", bookingRouter);
 
 app.listen(port, () =>
   console.log(`Server is listing at http://localhost:${port}`)
