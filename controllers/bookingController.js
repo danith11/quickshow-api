2715acba8b83bc5d0c40fb342cb554cf2404ea50
Yethum Danith <yethumdanith2002@gmail.com>
2025-08-30 10:56:54 +0530

    Stripe payment initialize

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import Show from "../models/Show.js";
 import Booking from "../models/Booking.js";
+import stripe from "stripe";
 
 // function to check availability of selected seats for a movie
 const checkSeatsAvailability = async (showId, selectedSeats) => {
@@ -48,7 +49,38 @@ export const createBooking = async (req, res) => {
 
     await showData.save();
 
-    res.json({ success: true, message: "Booked Succesfully" });
+    // Stripe gateway initialize
+    const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+
+    // Creating line items for stripe
+    const line_items = [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: showData.movie.title,
+          },
+          unit_amount: Math.floor(booking.amount) * 100,
+        },
+        quantity: 1,
+      },
+    ];
+
+    const session = await stripeInstance.checkout.sessions.create({
+      success_url: `${origin}/loading/my-bookings`,
+      cancel_url: `${origin}/my-bookings`,
+      line_items: line_items,
+      mode: "payment",
+      metadata: {
+        bookingId: booking._id.toString(),
+      },
+      expires_at: Math.floor(Date.now() / 1000 + 30 * 60), //Expires in 30 minutes
+    });
+
+    booking.paymentLink = session.url;
+    await booking.save();
+
+    res.json({ success: true, url: session.url });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
