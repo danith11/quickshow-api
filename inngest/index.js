195dcd8151efef40a3eff3127b121ea1b0b9ec64
Yethum Danith <yethumdanith2002@gmail.com>
2025-09-23 11:45:13 +0530

    Release booking if the payment is not paid

--- inngest/index.js
+++ inngest/index.js
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import Booking from "../models/Booking.js";
+import Show from "../models/Show.js";
 
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
@@ -19,7 +21,7 @@ const syncUserCreation = inngest.createFunction(
       name: first_name + " " + last_name,
       image: image_url,
     };
-    
+
     await User.create(userData);
   }
 );
@@ -51,4 +53,29 @@ const syncUserUpdation = inngest.createFunction(
   }
 );
 
-export const functions = [syncUserCreation, syncUserDeletion,syncUserUpdation];
+//ingest function to cancel booking and release the seats if the payment is not paid
+const releaseSeatsAndDeleteBooking = inngest.createFunction(
+  { id: "release-seats-delete-booking" },
+  { event: "app/checkpayment" },
+  async ({ event, step }) => {
+    const tenMinutesLater = new Date(Date.now() + 10 * 60 * 1000);
+    await step.sleepUntil("wait-for-10-minutes", tenMinutesLater);
+
+    await step.run("check-payment-status", async () => {
+      const bookingId = event.data.bookingId;
+      const booking = await Booking.findById(bookingId);
+
+      //If payment is not made release seats and delete bookings
+      if (!booking.isPaid) {
+        const show = await Show.findById(booking.show);
+        booking.bookedSeats.forEach(() => {
+          delete show.occupiedSeats[seat];
+        });
+        show.markModified("occupiedSeats");
+        await show.save();
+      }
+    });
+  }
+);
+
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdation];
